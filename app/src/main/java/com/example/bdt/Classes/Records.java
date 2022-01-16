715f0ce9b299a12bf1df25d71ed232d07366f7da
Srawas1996 <salim96tr@gmail.com>
2022-01-15 20:17:49 -0600

    this is Salim commit

---
+++ app/src/main/java/com/example/bdt/Classes/Records.java
@@ -0,0 +1,22 @@
+package com.example.bdt.Classes;
+
+public class Records {
+
+    private String Date;
+
+    public Records(String date) {
+        Date = date;
+    }
+
+    public Records() {
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+}
+
